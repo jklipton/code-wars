6c85ba1d862711a6e696f509eb3aecaf407c0695
Jen Lipton <jkl.lipton@gmail.com>
2018-03-06 17:26:35 -0800

    Delete abbreviatetwowords.js

--- abbreviatetwowords.js
+++
@@ -1,7 +0,0 @@
-function abbrevName(name){
-    const space = name.indexOf(' ')
-    const second = name.charAt(space+1).toUpperCase();
-    return `${name.charAt(0).toUpperCase()}.${second}`;
-};
-
-abbrevName("Sam Harris");
